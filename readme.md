2810c2c5dd968c72cf19888c29e9c254c2177fb8
edouard <edouard.seemann@epitech.eu>
2022-12-07 06:59:17 -0500

    [ADD] add basic template commands

---
+++ readme.md
@@ -0,0 +1,62 @@
+# Template
+
+Template is a CLI template manager.
+
+With template you can create and load templates.
+
+---
+# Usage
+
+## list templates :
+
+To list all templates run:
+```bash
+template list
+```
+this will list all templates in the template directory.
+
+
+## Create template :
+
+creating a template is easy, just go to the directory you want to create a template from and run:
+```bash
+template create
+```
+this will open a prompt asking you for template configuration, after you are done, the template will be created.
+
+
+## Load template :
+
+Go to the directory you want to load the template to and run:
+```bash
+template load <template name>
+```
+this will load the template to the current directory.
+note that template name is specified in the template configuration file while creating the template.
+
+## Remove template :
+    
+To delete a template run:
+```bash
+template remove <template name>
+```
+this will delete the template from the template directory.
+note that template name is specified in the template configuration file while creating the template.
+
+---
+
+# Installation
+1. clone the repo <br>
+    `git clone https://github.com/edoigtrd/template.git`
+
+2. cd into the repo<br>
+    `cd template`
+
+3. make sure bin/template is executable <br>
+    `chmod +x bin/template`
+
+4. add /bin to your path <br>
+    `export PATH=$PATH:$(pwd)/bin`
+
+5. make path modifications permanent by adding the above line to your .bashrc or .zshrc file <br>
+    `echo "export PATH=$PATH:$(pwd)/bin" >> ~/.bashrc`
---
+++ template.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+import os
+import sys
+import json
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argv.pop(0)
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    wd = os.getcwd()
+    # check if templates.json exists
+    if not os.path.exists(script_path+"/templates.json"):
+        # if it doesn't, create it
+        open(script_path+"/templates.json", "w").write("[]")
+
+    # if no arguments are passed, show help
+    if len(argv) == 0:
+        argv.append("help")
+
+    if argv[0] == "list":
+        # load templates.json
+        templates = json.loads(open(script_path+"/templates.json").read())
+        for template in templates:
+            # for each template, print name and description
+            print(template["name"])
+            print(f"\t{template['description']}")
+            print()
+    elif argv[0] == "help":
+        # load help.txt and print it
+        print(open(script_path+"/help.txt").read())
+
+    elif argv[0] == "load":
+        # load templates.json
+        templates = json.loads(open(script_path+"/templates.json").read())
+        template = None
+        # search for template with name argv[1]
+        for t in templates:
+            if t["name"] == argv[1]:
+                template = t
+                break
+        # if template is not found, print error and exit
+        if template == None:
+            print(f"Template {argv[1]} not found")
+            exit(1)
+        else:
+            # if template is found, copy template directory to current directory
+            os.system(
+                f"cp -r \"{script_path}/templates/{template['dir']}/.\" .")
+            # if after command is not empty, run it
+            if t["after"] != None or t["after"] != "":
+                os.system(t["after"])
+
+    elif argv[0] == "create":
+        # load templates.json
+        d = json.loads(open(script_path+"/templates.json").read())
+        # ask user for template name, description, directory and after command
+        p_name = input("Template name: ")
+        p_desc = input("Template description: ")
+        p_dir = input("Template directory: ")
+        p_after = input("After command: ")
+
+        # check if template with same name already exists
+        for t in d:
+            if t["name"] == p_name:
+                # if it does, ask user if he wants to overwrite it
+                print("Template with same name already exists")
+                if input("Overwrite? [y/N] ").lower() == "y":
+                    d.remove(t)
+                else:
+                    exit(0)
+        # add template to templates.json
+        d.append({
+            "name": p_name,
+            "description": p_desc,
+            "dir": p_dir,
+            "after": p_after
+        })
+        open(script_path+"/templates.json", "w").write(json.dumps(d, indent=4))
+        # create template directory
+        os.system(f"mkdir \"{script_path}/templates/{p_dir}\"")
+        # copy current directory to template directory
+        os.system(f"cp -r \"{wd}/.\" \"{script_path}/templates/{p_dir}\"")
+        print(f"Template {p_name} created")
+
+    elif argv[0] == "remove":
+        if len(argv) == 1:
+            # if no template name is passed, print error and exit
+            print("Missing argument: template name")
+            exit(1)
+        # load templates.json
+        d = json.loads(open(script_path+"/templates.json").read())
+        # search for template with name argv[1]
+        for t in d:
+            if t["name"] == argv[1]:
+                # if it is found, ask user if he wants to delete it
+                print(f"Template {argv[1]} found")
+                if input("Delete? [y/N] ").lower() == "y":
+                    # if he wants to delete it, delete it
+                    d.remove(t)
+                    open(script_path+"/templates.json", "w").write(
+                        json.dumps(d, indent=4))
+                    # delete template directory
+                    os.system(
+                        f"rm -rf \"{script_path}/templates/{t['dir']}\"")
+                    print(f"Template {argv[1]} deleted")
+                    exit(0)
+                else:
+                    exit(0)
+        # if template is not found, print error and exit
+        print(f"Template {argv[1]} not found")
+        exit(1)
+
+    elif argv[0] == "edit":
+        if len(argv) == 1:
+            # if no template name is passed, print error and exit
+            print("Missing argument: template name")
+            exit(1)
+        # load templates.json
+        d = json.loads(open(script_path+"/templates.json").read())
+        # search for template with name argv[1]
+        for t in d:
+            if t["name"] == argv[1]:
+                # if it is found, ask user for new template name, description, directory and after command
+                # get actual values as default
+                default = {
+                    "name": t["name"],
+                    "description": t["description"],
+                    "dir": t["dir"],
+                    "after": t["after"]
+                }
+                print(f"Template {argv[1]} found")
+                print(f"Leave blank to keep actual values")
+                p_name = input(f"Template name ({default['name']}): ") or default["name"]
+                p_desc = input(f"Template description: ({default['description']})") or default["description"]
+                p_dir = input(f"Template directory: ({default['dir']})") or default["dir"]
+                p_after = input(f"After command: ({default['after']})") or default["after"]
+                # asks user if he wants to save changes
+                print("New values:")
+                print(f"\tName: {p_name}")
+                print(f"\tDescription: {p_desc}")
+                print(f"\tDirectory: {p_dir}")
+                print(f"\tAfter command: {p_after}")
+                # check if template with same name already exists
+                for c in d:
+                    if c["name"] == p_name and c != t:
+                        print("Template with same name already exists, aborting")
+                        print(f"If you want to change the name, delete the template {p_name} first")
+                        exit(1)
+                if input("Save? [y/N] ").lower() == "y":
+                    # if he wants to save changes, save them
+                    t["name"] = p_name
+                    t["description"] = p_desc
+                    t["dir"] = p_dir
+                    t["after"] = p_after
+                    open(script_path+"/templates.json", "w").write(
+                        json.dumps(d, indent=4))
+                    # rename template directory
+                    # check if template name changed
+                    if default["dir"] != p_dir:
+                        os.system(
+                            f"mv \"{script_path}/templates/{default['dir']}\" \"{script_path}/templates/{p_dir}\"")
+                    print(f"Template {argv[1]} edited")
+                    exit(0)
+        print(f"Template {argv[1]} not found, cannot edit")
+        exit(1)
+    elif argv[0] == "path":
+        # print path to templates directory
+        print(script_path+"/templates")
+    else:
+        print("Unknown command")
+        print("use `template help` for help")
+        exit(1)
