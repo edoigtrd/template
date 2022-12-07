c64f690d4e37afb1645660d7622d32ded5eadb00
edouard <edouard.seemann@epitech.eu>
2022-12-07 09:34:48 -0500

    [FIX] added quotes to paths in git -C

--- template.py
+++ template.py
@@ -30,7 +30,7 @@ def check_for_update() :
     # if local hash is different from remote hash, update
     if local_hash != remote_hash:
         print("Updating...")
-        os.system(f"git -C {script_path} pull")
+        os.system(f"git -C \"{script_path}\" pull")
         print("Updated")
         return True
     else :
